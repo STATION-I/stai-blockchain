d6cd96f12ccb4934d6ff4cbda5a1fab103e638b5
STATION-I <89520950+STATION-I@users.noreply.github.com>
2021-10-09 01:01:29 +0200

    Update FarmOverviewHero.tsx

--- staicoin-blockchain-gui/src/components/farm/overview/FarmOverviewHero.tsx
+++ staicoin-blockchain-gui/src/components/farm/overview/FarmOverviewHero.tsx
@@ -36,7 +36,7 @@ export default function FarmOverviewHero() {
               where your farm will be once you add a plot.{' '}
               <Link
                 target="_blank"
-                href="https://github.com/pinksheetscrypto/staicoin-blockchain/wiki/Network-Architecture"
+                href="https://github.com/STATION-I/staicoin-blockchain/wiki/Network-Architecture"
               >
                 Learn more
               </Link>
