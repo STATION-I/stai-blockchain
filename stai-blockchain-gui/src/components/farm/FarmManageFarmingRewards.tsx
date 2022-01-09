5e9472a7755283ad07cb2d3b6fc1e4c23981ddae
Pierre FAURE <em@iguru.team>
2022-01-09 12:51:50 +0100

    GUI small fixes

--- stai-blockchain-gui/src/components/farm/FarmManageFarmingRewards.tsx
+++ stai-blockchain-gui/src/components/farm/FarmManageFarmingRewards.tsx
@@ -188,7 +188,7 @@ export default function FarmManageFarmingRewards(props: Props) {
                   <Trans>
                     Note that this does not change your pooling payout
                     addresses. This only affects old format plots, and the
-                    0.25STAI reward for pooling plots.
+                    1 STAI reward for pooling plots.
                   </Trans>
                 </Typography>
               </>
